5c5468fe6205a260e790f2b31235c4b6ff983805
liangtq <liang_tian_qi@live.cn>
2014-09-07 17:03:02 -0500

    forth commit on location

--- src/com/ece4600/mainapp/DisplayWifi.java
+++ src/com/ece4600/mainapp/DisplayWifi.java
@@ -3,12 +3,14 @@ package com.ece4600.mainapp;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.ece4600.mainapp.location.WifiScan;
@@ -25,6 +27,7 @@ public class DisplayWifi extends Activity implements WifiScanListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_wifi);
+        setupMessageButtonreturn();
         
         wifi = new WifiScan(this);
         wifiInfo = (TextView) findViewById(R.id.wifi_info);
@@ -34,7 +37,20 @@ public class DisplayWifi extends Activity implements WifiScanListener {
 		refreshAccessPoints();
     }
     
-    public void refreshAccessPoints(View view) {
+    private void setupMessageButtonreturn() {
+		// TODO Auto-generated method stub
+    	Button messageButton = (Button)findViewById(R.id.return_location);
+    	messageButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//Toast.makeText(Location.this, "Return to profile", Toast.LENGTH_LONG).show();
+				startActivity(new Intent(DisplayWifi.this, Location.class));
+				finish();
+			}
+		});	
+	}
+
+	public void refreshAccessPoints(View view) {
     	refreshAccessPoints();
     }
     
