abfc4a3c5c849dc00ab359c240642f3d72003c06
Yang <umsu2@myumanitoba.ca>
2015-01-01 12:57:09 -0600

    added mapviewing capability for location system. relocated the previous map codes into location_old_jan_1

--- src/com/ece4600/mainapp/Location.java
+++ src/com/ece4600/mainapp/Location.java
@@ -22,216 +22,54 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.qozix.tileview.TileView;
 
-//edited by tianqi
+import android.widget.ImageView;
+
 public class Location extends Activity {
 
-	private Button messageButton;
-	private TextView allNetWork;
-	private Button start;
-	private Button stop;
-	private Button check;
-	private Button output;
+    TileView tileView;
 
-	private WifiAdmin mWifiAdmin;
-	private List<ScanResult> list;
-	private ScanResult mScanResult;
-	private StringBuffer sb = new StringBuffer();
-	private EditText roomnm;
-	private EditText spotnm;
-	
-	private String filename = "fingerprint.xml";
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_location);
-		setupMessageButton1();
-		mWifiAdmin = new WifiAdmin(Location.this);
-		init();
-	}
-	
-	
-	public void init() {
-		messageButton = (Button)findViewById(R.id.scan);
-		allNetWork = (TextView) findViewById(R.id.allNetWork);
-		start = (Button) findViewById(R.id.start);
-		stop = (Button) findViewById(R.id.stop);
-		check = (Button) findViewById(R.id.check);
-		output = (Button) findViewById(R.id.output);
-		roomnm = (EditText) findViewById(R.id.roomnm);
-		spotnm = (EditText) findViewById(R.id.spotnm);
-		start.setOnClickListener(new MyListener());
-		stop.setOnClickListener(new MyListener());
-		check.setOnClickListener(new MyListener());
-		output.setOnClickListener(new MyListener());
-		messageButton.setOnClickListener(new MyListener());
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	
-	
-	private class MyListener implements OnClickListener {
+        // Create our TileView
+        tileView = new TileView(this);
 
-		@Override
-		public void onClick(View v) {
-			switch (v.getId()) {
-			case R.id.scan:
-				getAllNetWorkList();
-				break;
-			case R.id.start:
-				mWifiAdmin.openWifi();
-				Toast.makeText(Location.this,
-						"Current WiFi Status is on" + mWifiAdmin.checkState(),
-						Toast.LENGTH_SHORT).show();
-				break;
-			case R.id.stop:
-				mWifiAdmin.closeWifi();
-				Toast.makeText(Location.this,
-						"Current WiFi Status is off" + mWifiAdmin.checkState(),
-						Toast.LENGTH_SHORT).show();
-				break;
-			case R.id.check:
+        // Set the minimum parameters
+        tileView.setSize(1157,848);
+        tileView.addDetailLevel(1f, "tiles/1000_%col%_%row%.png", "downsamples/map.png");
+        tileView.addDetailLevel(0.5f, "tiles/500_%col%_%row%.png", "downsamples/map.png");
+        tileView.addDetailLevel(0.25f, "tiles/250_%col%_%row%.png", "downsamples/map.png");
+        tileView.addDetailLevel(0.125f, "tiles/125_%col%_%row%.png", "downsamples/map.png");
+        // Add the view to display it
+        setContentView(tileView);
+        // use pixel coordinates to roughly center it
+        // they are calculated against the "full" size of the mapView 
+        // i.e., the largest zoom level as it would be rendered at a scale of 1.0f
+        tileView.moveToAndCenter( 1157,848 );
+        tileView.slideToAndCenter( 1157,848 );
 
-				String room = roomnm.getText().toString();
-				String spot = spotnm.getText().toString();
-				if (list != null) {
+        // Set the default zoom (zoom out by 4 => 1/4 = 0.25)
+        tileView.setScale( 0.25 );
+        
+        ImageView markerA = new ImageView(this);
+        markerA.setImageResource(R.drawable.maps_marker_blue);
+        markerA.setTag("Nice");
 
-					// we have to bind the new file with a FileOutputStream
-					FileOutputStream fileos = null;
-					try {
-						fileos = openFileOutput("fingerprint.xml",
-								Context.MODE_APPEND);
-					} catch (FileNotFoundException e) {
-						Log.e("FileNotFoundException",
-								"can't create FileOutputStream");
-					}
-					// we create a XmlSerializer in order to write xml data
-					XmlSerializer serializer = Xml.newSerializer();
-					try {
-						// we set the FileOutputStream as output for the
-						// serializer, using UTF-8 encoding
-						serializer.setOutput(fileos, "UTF-8");
-						// Write <?xml declaration with encoding (if encoding
-						// not null) and standalone flag (if standalone not
-						// null)
-						// serializer.startDocument(null,
-						// Boolean.valueOf(true));
-						// set indentation option
-						serializer
-								.setFeature(
-										"http://xmlpull.org/v1/doc/features.html#indent-output",
-										true);
-						// start a tag called "root"
-						serializer.startTag(null, "WifiInfo");
-						// i indent code just to have a view similar to xml-tree
+        ImageView markerB = new ImageView(this);
+        markerB.setImageResource(R.drawable.maps_marker_blue);
+        markerB.setTag("Paris");
 
-						serializer.startTag("", "Room");
-						serializer.text(room);
-						serializer.endTag("", "Room");
-						serializer.startTag("", "Spot");
-						serializer.text(spot);
-						serializer.endTag("", "Spot");
-
-						if (list != null) {
-							for (int i = 0; i < list.size(); i++) {
-								
-								mScanResult = list.get(i);
-								String mac = mScanResult.BSSID;
-								String level = "" + mScanResult.level;
-								serializer.startTag("", "BSSID");
-								serializer.text(mac);
-								serializer.endTag("", "BSSID");
-								serializer.startTag("", "level");
-								serializer.text(level);
-								serializer.endTag("", "level");
-							}
-						}
-
-						serializer.endTag(null, "WifiInfo");
-
-						// serializer.endDocument();
-						// write xml data into the FileOutputStream
-						serializer.flush();
-						// finally we close the file stream
-						fileos.close();
-
-					} catch (Exception e) {
-						Log.e("Exception",
-								"error occurred while creating xml file");
-					}
-					Toast.makeText(
-							Location.this,
-							"FingerPrint has been written into XML file"
-									+ filename, Toast.LENGTH_SHORT).show();
-				} else {
-					Toast.makeText(
-							Location.this,
-							"There is no Fingerprint data available currently, please click the scan button",
-							Toast.LENGTH_SHORT).show();
-				}
-				break;
-			case R.id.output:
-
-				String oldfile = getFilesDir() + "/fingerprint.xml";
-			
-				File file = new File(oldfile);
-				file.delete();
-				break;
-			default:
-				break;
-			}
-
-		}
-	
-	}
-	
-	
-	public void getAllNetWorkList() {
-	
-		if (sb != null) {
-			sb = new StringBuffer();
-		}
-	
-		mWifiAdmin.startScan();
-		list = mWifiAdmin.getWifiList();
-		if (list != null) {
-			for (int i = 0; i < list.size(); i++) {
-				
-				mScanResult = list.get(i);
-				sb = sb.append(mScanResult.BSSID + "  ")
-						.append(mScanResult.SSID + "   ")
-						.append(mScanResult.capabilities + "   ")
-						.append(mScanResult.frequency + "   ")
-						.append(mScanResult.level + "\n\n");
-			}
-			allNetWork.setText("WiFi Scan Result\n" + sb.toString());
-		}
-	}
-
-	
-	
-	private void setupMessageButton1(){
-    	Button messageButton = (Button)findViewById(R.id.returnloca);
-    	messageButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				//Toast.makeText(Location.this, "Return to profile", Toast.LENGTH_LONG).show();
-				startActivity(new Intent(Location.this, MainActivity.class));
-				finish();
-			}
-		});	
+        tileView.addMarker(markerA, 100, 200, -0.5f, -1.0f);
+        tileView.addMarker(markerB, 300, 500, -0.5f, -1.0f);
     }
-	
-	public void onBackPressed() {
-		// do something on back.return;		
-		startActivity(new Intent(Location.this, MainActivity.class));
-		finish();
-	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.location, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
@@ -240,25 +78,11 @@ public class Location extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		super.onOptionsItemSelected(item);
-    	switch(item.getItemId()){
-    	case R.id.locamenu_pedo:
-    		startActivity(new Intent(this, Pedometer.class));
-    		finish();
-    		break;
-    	case R.id.locamenu_heart:
-    		startActivity(new Intent(this, Heartrate.class));
-    		finish();
-    		break;
-    	case R.id.locamenu_post:
-    		startActivity(new Intent(this, Posture.class));
-    		finish();
-    		break;
-    	}
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return true; 
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
+
